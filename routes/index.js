22c4bddcd360870571a3754bc7d419277edcbb2b
Jacob <vernonjacoba@yahoo.com>
2018-10-14 13:08:14 -0600

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var blog = document.querySelector("#blog");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Pokeblog Posts' });
+});
+submit.addEventListener("click", submits);
+submit.addEventListener("Enter", submits);
+
+module.exports = router;
