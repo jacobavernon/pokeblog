174f915bf77fdc9996b843e1880355efbfac731d
Marcos Gonzales <markymarrk@Marcoss-MacBook-Pro.local>
2018-10-14 14:12:53 -0600

    Add user id route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+const users = [
+  {
+    1: {
+      name: 'marquitos',
+      occ: 'noob-slayer',
+    }
+  }
+]
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Pokeblog Posts' });
+router.send('/:id', (req, res) => {
+  res.render('users'), {
+    id: users[req.params.id].name
+  }
 });
 
 module.exports = router;
