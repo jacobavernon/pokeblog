92e0715ef5b589bfc60e31ba3912e54aecf0f007
Jacob <vernonjacoba@yahoo.com>
2018-10-29 19:10:46 -0600

    blog

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,20 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Pokeblog Posts' });
+  var blog = document.querySelector("#blog");
+  var blogprinter = document.querySelector("#blogprinter");
+  var submits = document.querySelector("#submits");
+  
+  submits.addEventListener("click", blogSubmit);
+  submits.addEventListener("Enter", blogSubmit);
+  submits.addEventListener("Enter", console.log('Enter worked'));
+  submits.addEventListener("click", console.log('click worked'));
+
+  function blogSubmit(){
+    blogprinter.innerHTML = `${blog.value}`;
+  }
+  console.log(blog.value);
+
 });
 
 module.exports = router;
