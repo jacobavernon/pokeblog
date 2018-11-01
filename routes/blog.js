b012cb485a90dc45a2f2e4456f983e5440432404
Marcos Gonzales <markymarrk@Marcoss-MacBook-Pro.local>
2018-10-31 19:52:09 -0600

    Cleaned up the code.

--- routes/blog.js
+++ routes/blog.js
@@ -12,17 +12,11 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/posts', (req, res, next) => {
-  var pokeBlog = {
-    author: req.body.author,
-    body: req.body.post,
-  }
-  res.send(pokeBlog.author + pokeBlog.body)
-  next();
-})
-
-router.get('/posts', (req, res, next) => {
   res.render('posts', {
+    author: req.body.author,
+    body: req.body.post
   })
+  next();
 })
 
 module.exports = router;
