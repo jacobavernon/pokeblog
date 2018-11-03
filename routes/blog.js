3be2ea3ce02c90774b1c45a44b05faeb5241123d
Jimmy Cleveland <jimmydalecleveland@gmail.com>
2018-11-03 16:57:57 -0600

    Styling of the blog form and a new route

--- routes/blog.js
+++ routes/blog.js
@@ -1,22 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-const pokeBlogs = {
-  author: 'marquitos',
-  body: 'I caught a mew today!'
-}
+const pokeBlogs = {}
 
 // This is our blog 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.render('blog')
 })
 
-router.post('/posts', (req, res, next) => {
+router.post('/posts', (req, res) => {
+  const { author, post } = req.body;
+  pokeBlogs[author] = { post };
+
   res.render('posts', {
     author: req.body.author,
     body: req.body.post
   })
-  next();
+})
+
+router.get('/posts/:author', (req, res, next) => {
+  // Does our pokeBlogs object contain the requested author?
+  if (pokeBlogs[req.params.author]) {
+    res.render('posts', { author: req.params.author, body: pokeBlogs[req.params.author].post })
+  } else {
+    next();
+  }
 })
 
 module.exports = router;
