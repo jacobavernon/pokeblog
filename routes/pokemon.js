3e418b64a7741dc9b06f5c286937d355c6d1c5cf
Marcos Gonzales <markymarrk@Marcoss-MacBook-Pro.local>
2018-11-14 12:42:23 -0700

    added pokemonController. Cleaned up pokemon route

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,67 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const axios = require('axios');
+var pokemonController = require("../controllers/pokemonController");
+
 
 //All Pokemon Page
-router.get('/', (req, res, next) => {
-  axios.get('https://pokeapi.co/api/v2/pokemon/')
-    .then((response) => {
-      const { results } = response.data
-      res.render('allPokemon', { 
-        pokemon: results.slice(0, 151)
-      })      
-  })
-  .catch((err) => {
-    next()
-  })
-})
+router.get('/', pokemonController.getAllPokemon) 
 
 //  Get indivual pokemon from API 
- router.get('/:id', (req, res, next) => {
-   axios
-     .get(`https://pokeapi.co/api/v2/pokemon/${req.params.id}`)
-     .then((response) => {
-       const pokemon = {
-        name: response.data.name,
-        sprites: response.data.sprites,
-        types: response.data.types,
-        weight: response.data.weight,
-        height: response.data.height,
-        abilities: response.data.abilities,
-        moves: response.data.moves,
-        search: req.query.pokemon
-       }
-       res.render('individualPokemon', { pokemon }) 
-    })
-    .catch((err) => {
-      next();
-    })
-  })
+router.get('/:id', pokemonController.getIndividualPokemon)
 
 // This is the search feature  
-router.get('/search/', (req, res, next) => {
-
-  axios
-      .get(`https://pokeapi.co/api/v2/pokemon/${req.query.pokemon}`)
-      .then((response) => {
-        const pokemon = {
-          name: response.data.name,
-          sprites: response.data.sprites,
-          types: response.data.types,
-          weight: response.data.weight,
-          height: response.data.height,
-          abilities: response.data.abilities,
-          moves: response.data.moves,
-          search: req.query.pokemon
-       }
-       res.render('individualPokemon', { 
-         pokemon: pokemon,
-
-       })
-         console.log(req.query.pokemon)
-    })
-    .catch((err) => {
-      next();
-    })
-})
+router.get('/search/', pokemonController.getSearchPokemon)
 module.exports = router;
