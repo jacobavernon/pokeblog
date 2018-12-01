b63fa79e2765fd0f962d3133890ca71c8a7ffa98
Marcos <markymarrk@gmail.com>
2018-12-01 14:10:40 -0700

    added comments

--- app.js
+++ app.js
@@ -12,9 +12,11 @@ var trainersRouter = require('./routes/trainers')
 
 var app = express();
 
-// view engine setup
+// Favicon setup
 app.use(favicon(path.join(__dirname, 'public/images/favicons', 'favicon.ico')));
 app.set('views', path.join(__dirname, 'views'));
+
+// View engine setup
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
